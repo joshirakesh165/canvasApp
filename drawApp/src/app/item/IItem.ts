184d8fecebd63a49c4cdf78891fec781e48a88c6
Rakesh Joshi <joshirakesh165@gmail.com>
2024-05-15 22:08:12 +0530

    Delete drawApp directory

--- drawApp/src/app/item/IItem.ts
+++
@@ -1,9 +0,0 @@
-export default interface IItem {
-    id:number,
-    expandable:boolean
-    name:string,
-    editable:boolean,
-    deletable: boolean,
-    type: 'file'| 'folder',
-    children:IItem[]
-}
--- drawApp/src/app/models/ICanvasConfig.ts
+++
@@ -1,6 +0,0 @@
-export default interface ICanvasConfig  {
-    selectedColor:string;
-    strokeSize:number;
-    clear:boolean,
-    tool:string
-}
