c640ad2b50f2fdee3d7de4ff7b8c3bf124811171
joshirakesh165 <joshirakesh165@gmail.com>
2024-05-15 22:12:54 +0530

    Code pushed from Api

---
+++ drawApp/src/app/scrachpad.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import ICanvasConfig from './models/ICanvasConfig';
+import { Socket, io } from 'socket.io-client';
+
+const URL = 'http://simple-draw.onrender.com';
+const CONFIG_CHANGE = 'configChange'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrachpadService {
+  socket: Socket;
+  defaultState = { selectedColor: 'black', tool: 'pencil', strokeSize: 1, clear: false }
+  private canvasConfig = new BehaviorSubject<ICanvasConfig>(this.defaultState)
+
+  constructor() {
+    this.socket = io(URL);
+    this.socket.on(CONFIG_CHANGE, (state) => this.canvasConfig.next(state))
+  }
+
+
+  public getSocketInstance = (): Socket => this.socket;
+  public getCanvasConfig = (): BehaviorSubject<ICanvasConfig> => this.canvasConfig;
+  public getConfigValue = (): ICanvasConfig => this.canvasConfig.value;
+
+  public setcanvasConfig(state: ICanvasConfig) : void {
+    this.canvasConfig.next(state);
+    this.socket.emit(CONFIG_CHANGE, state);
+  }
+
+  public resetDefaultState(): void {
+    this.canvasConfig.next(this.defaultState);
+    this.socket.emit(CONFIG_CHANGE, this.defaultState);
+  }
+}
